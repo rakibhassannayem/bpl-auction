a682be3ee9eaef518386996681f81af2fba8c806
rakibhassannayem <rakibhassannayem@gmail.com>
2025-09-21 23:58:51 +0600

    cards done

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,11 +1,50 @@
-import React, { use } from 'react';
+import { use } from "react";
+import userIcon from "../../assets/user.png";
+import flagIcon from "../../assets/flagIcon.png";
 
-const AvailablePlayers = ({playersPromise}) => {
-  const playerData = use(playersPromise)
-  console.log(playerData)
+const AvailablePlayers = ({ playersPromise }) => {
+  const playerData = use(playersPromise);
   return (
-    <div>
-      <h1>Available</h1>
+    <div className="max-w-7xl mx-auto grid md:grid-cols-3 gap-3">
+      {playerData.map((player) => (
+        <div className="card bg-base-100 shadow-sm p-4">
+          <img className="rounded-lg w-full h-60 object-cover" src={player.img} alt="Shoes" />
+
+          <div className="card-body">
+            <div className="flex items-center gap-1">
+              <img className="w-5 h-5" src={userIcon} alt="" />
+              <h2 className="card-title">{player.name}</h2>
+            </div>
+
+            <div className="flex items-center justify-between border-b border-gray-200 pb-3">
+              <div className="flex gap-1 items-center text-gray-400 font-semibold">
+                <img className="w-4 h-4" src={flagIcon} alt="" />
+                <span>{player.country}</span>
+              </div>
+              <h3 className="font-semibold text-gray-600 bg-gray-100 rounded-xl px-3 py-2">
+                {player.role}
+              </h3>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <h3 className="font-semibold">Rating</h3>
+              <h3 className="font-semibold text-gray-500">{player.rating}</h3>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <h3 className="font-semibold">{player.battingStyle}</h3>
+              <h3 className="font-semibold text-gray-500">{player.bowlingStyle}</h3>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <h3 className="font-semibold">
+                Price: $<span>{player.price}</span>
+              </h3>
+              <button className="btn">Choose Player</button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
